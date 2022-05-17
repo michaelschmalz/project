1a08b8d8cd71bb3706a4697b6c85fc5752f72145
michaelschmalz <97465778+michaelschmalz@users.noreply.github.com>
2022-05-17 14:05:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+// function initMap(): void {
+//
+//   const birmingham = { lat: 33.52347, lng: 33.52347 };
+//
+//   const map = new google.maps.Map(
+//     document.getElementById("map") as HTMLElement,
+//     {
+//       zoom: 4,
+//       center: birmingham,
+//     }
+//   );
+//
+//   const marker = new google.maps.Marker({
+//     position: birmingham,
+//     map: map,
+//   });
+// }
+//
+// declare global {
+//   interface Window {
+//     initMap: () => void;
+//   }
+// }
+// window.initMap = initMap;
